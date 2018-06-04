5f25a38a32e1ddf23afca10f186c505146011edd
NinjaCracker <o.olaf@hotmail.com>
2018-06-04 11:36:59 +0200

    Login done

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -16,8 +16,10 @@
         </column>
         <column md="3"></column>
       </row>
-      <btn class="z-depth-5" color="red"  @click.native="login()">Login</btn>
-      <btn class="z-depth-5" color="red"  @click.native="register()">Register</btn>
+        <btn class="z-depth-5" color="red"  @click.native="login()">Login</btn>
+      <router-link to="/Register">
+        <btn class="z-depth-5" color="red"  href="/Register">Register</btn>
+      </router-link>
        <div v-if="showResponse"><h1>User created: {{ response }}</h1></div>
     </container>
   </div>
