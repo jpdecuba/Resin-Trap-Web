3b4a80f784db1611c58df9454b222b27efc4a9d2
Dotter <bart.van.dongen@hotmail.com>
2018-06-06 11:28:06 +0200

    backend get logs

--- backend/src/main/java/Controller/BackendController.java
+++ backend/src/main/java/Controller/BackendController.java
@@ -1,11 +1,14 @@
 package Controller;
 
+import Shared.Logging.LogConnection;
 import Shared.Model.User;
 import Sockets.SocketClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Set;
 
 @RestController()
 @RequestMapping("/api")
@@ -31,5 +34,13 @@ public class BackendController {
 		boolean create = client.Register(usr);
 		return create;
 	}
-	
+
+
+	@RequestMapping(path = "/logs", method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.CREATED)
+	public @ResponseBody Set<LogConnection> Getlogs (@RequestBody User usr) {
+		Set<LogConnection> create = client.GetlogFiles(usr);
+		return create;
+	}
+
 }
